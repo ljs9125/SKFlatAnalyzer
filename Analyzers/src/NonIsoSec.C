9bad7741b13c09d61ad8d91df60e8ad0c86da00d
ljs9125 <ljs9125@snu.ac.kr>
2019-10-26 20:37:59 +0900

    NonIsoSec.C==>File Directory Changed

--- Analyzers/src/NonIsoSec.C
+++ Analyzers/src/NonIsoSec.C
@@ -242,14 +242,14 @@ void NonIsoSec::executeEventFromParameter(AnalyzerParameter param){
     //Second Lepton RelIso>0.3 cut
     if(muons.at(1).RelIso()>0.3){
       if(muons.at(0).Charge()*muons.at(1).Charge()>0){
-        FillHist(param.Name+"NonIso/Mll_SS_DATA", Mass.M(), weight, 3000, 0., 3000.);
+        FillHist("NonIso/"+param.Name+"/Mll_SS_DATA", Mass.M(), weight, 3000, 0., 3000.);
 
         if(muons.at(0).Charge() > 0) { 
-          FillHist(param.Name+"NonIso/Mll_pp_DATA", Mass.M(), weight, 3000, 0., 3000.);
+          FillHist("NonIso/"+param.Name+"/Mll_pp_DATA", Mass.M(), weight, 3000, 0., 3000.);
         }
      
         else {
-          FillHist(param.Name+"NonIso/Mll_mm_DATA", Mass.M(), weight, 3000, 0., 3000.);
+          FillHist("NonIso"+param.Name+"/Mll_mm_DATA", Mass.M(), weight, 3000, 0., 3000.);
         }
       }  
     }
